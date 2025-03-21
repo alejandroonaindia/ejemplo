5f002df59741c0d28cf62afd02172fa5c880fe1f
ikasle <ikasle@DAM1-02.arangoya.local>
2025-03-21 13:22:50 +0100

    version 1.0.3

--- Ejemplo2/src/Main/main.java
+++ Ejemplo2/src/Main/main.java
@@ -4,6 +4,6 @@ public class main {
 	public main() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Que chungo");
-		System.out.println("Jurgi estuvo aqui");
+		System.out.println("Jurgi estuvo aquii");
 	}
 }
